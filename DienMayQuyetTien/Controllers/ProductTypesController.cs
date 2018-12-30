29038e7d6b661f0e1b4bf9979544122dd98d9cee
sieusjnc698 <sieusjnc698@gmail.com>
2018-12-31 04:08:16 +0700

    Edit Home Index

---
+++ DienMayQuyetTien/Controllers/ProductTypesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DienMayQuyetTien.Models;
+
+namespace DienMayQuyetTien.Controllers
+{
+    public class ProductTypesController : Controller
+    {
+        private DmQT07Entities1 db = new DmQT07Entities1();
+
+        // GET: ProductTypes
+        public ActionResult Index()
+        {
+            return View(db.ProductTypes.ToList());
+        }
+        public ActionResult ProductType(int id)
+        {
+            var type = db.ProductTypes.FirstOrDefault(o => o.ID == id);
+            LoaiSP pt = new LoaiSP();
+            pt.ProductTypeName = type.ProductTypeName;
+            pt.PRODUCT = type.Products.ToList();
+            return View(pt);
+        }
+        // GET: ProductTypes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductType productType = db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productType);
+        }
+
+        // GET: ProductTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,ProductTypeCode,ProductTypeName")] ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductTypes.Add(productType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productType);
+        }
+
+        // GET: ProductTypes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductType productType = db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productType);
+        }
+
+        // POST: ProductTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,ProductTypeCode,ProductTypeName")] ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productType);
+        }
+
+        // GET: ProductTypes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductType productType = db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productType);
+        }
+
+        // POST: ProductTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProductType productType = db.ProductTypes.Find(id);
+            db.ProductTypes.Remove(productType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
