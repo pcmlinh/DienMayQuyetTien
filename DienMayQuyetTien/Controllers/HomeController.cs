c51a65c66a43a38b2da50815c7761e1d7eeb0787
pcmlinh <pcmlinh1112@gmail.com>
2018-12-30 14:19:26 +0700

    List product home index

--- DienMayQuyetTien/Controllers/HomeController.cs
+++ DienMayQuyetTien/Controllers/HomeController.cs
@@ -1,18 +1,26 @@
-﻿using System;
+﻿using DienMayQuyetTien.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
 
 namespace DienMayQuyetTien.Controllers
 {
     public class HomeController : Controller
     {
+        DmQT07Entities1 db = new DmQT07Entities1();
         public ActionResult Index()
         {
-            return View();
+            var product = db.Products.OrderByDescending(x => x.ID).ToList();
+            return View(product);
         }
-
+        public FileResult Details(int id)
+        {
+            var path = Server.MapPath("~/Images/" + id);
+            return File(path, "images");
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
